14b958b466445a81b1e81db2a65d9b7721a13a32
poneciak57 <poneciak@gmail.com>
2025-03-18 23:04:57 +0100

    added input standarization
    added poorly implemented standarization, will fix it later but works fine

--- src/regressor.rs
+++ src/regressor.rs
@@ -7,9 +7,16 @@ pub enum Error {
     AlreadyFitted,
 }
 
+/// A simple linear regression model using gradient descent.
+/// This model can be used to fit a linear function to a set of input data and target values.
+/// The model is trained using gradient descent, and the weights and bias are updated iteratively.
+/// The model can be used to make predictions on new input data after it has been fitted.
 pub struct LinearRegressor {
     bias: f64,
+    
     weights: Vec<f64>,
+    features_mean: Vec<f64>,
+    features_std: Vec<f64>,
 
     max_epochs: usize,
     loss_threshold: f64,
@@ -19,9 +26,12 @@ pub struct LinearRegressor {
     fitted: bool,
 }
 impl LinearRegressor {
+    /// Creates a new instance of the LinearRegressor.
     pub fn new() -> Self {
         Self {
             weights: vec![],
+            features_mean: vec![],
+            features_std: vec![],
             bias: 0.0,
             max_epochs: 1_000_000,
             loss_threshold: 0.0001,
@@ -31,6 +41,14 @@ impl LinearRegressor {
         }
     }
 
+    /// Fits the model to the given input data and target values.
+    /// # Arguments
+    /// * `x` - A 2D vector of input data.
+    /// * `y` - A vector of target values.
+    /// # Returns
+    /// The final loss value.
+    /// # Errors
+    /// Returns an error if the model is already fitted or if the input data is invalid.
     pub fn fit(&mut self, x: &[Vec<f64>], y: &[f64]) -> Result<f64, Error> { 
         if self.fitted {
             return Err(Error::AlreadyFitted);
@@ -43,7 +61,7 @@ impl LinearRegressor {
         if (y.len() != n_samples) || (x.iter().any(|row| row.len() != n_features)) {
             return Err(Error::InvalidInput);
         }
-
+        let standardized_x = self.standarize(x);
         self.weights = vec![1.0; n_features];
 
         // Gradient descent loop
@@ -54,7 +72,7 @@ impl LinearRegressor {
             
             // Loss function
             let mut loss = 0.0; 
-            for (j, sample) in x.iter().enumerate() {
+            for (j, sample) in standardized_x.iter().enumerate() {
                 let target: f64 = y[j];
 
                 // Prediction
@@ -99,7 +117,7 @@ impl LinearRegressor {
         
         // Compute final loss
         let mut loss = 0.0;
-        for (j, sample) in x.iter().enumerate() {
+        for (j, sample) in standardized_x.iter().enumerate() {
             let target: f64 = y[j];
 
             // Prediction
@@ -125,7 +143,14 @@ impl LinearRegressor {
             return Err(Error::NotFitted);
         }
         let mut predictions: Vec<f64> = vec![];
-        for inp in x.iter() {
+        let standardized_x = x.iter().map(|sample| {
+            let mut standardized = vec![0.0; sample.len()];
+            for (i, &value) in sample.iter().enumerate() {
+                standardized[i] = (value - self.features_mean[i]) / self.features_std[i];
+            }
+            standardized
+        }).collect::<Vec<_>>();
+        for inp in standardized_x.iter() {
             let mut pred: f64 = self.bias;
             for (i, &weight) in self.weights.iter().enumerate() {
                 pred += weight * inp[i];
@@ -133,6 +158,31 @@ impl LinearRegressor {
             predictions.push(pred);      
         }
         Ok(predictions)
+    }
+
+
+    /// TODO change how it works one function does too much
+    fn standarize(&mut self, x: &[Vec<f64>]) -> Vec<Vec<f64>> {
+        self.features_mean = vec![0.0; x[0].len()];
+        self.features_std = vec![0.0; x[0].len()];
+        for (i, sample) in x.iter().enumerate() {
+            for (j, &value) in sample.iter().enumerate() {
+                self.features_mean[j] += value;
+                self.features_std[j] += value * value;
+            }
+        }
+        for j in 0..self.features_mean.len() {
+            self.features_mean[j] /= x.len() as f64;
+            self.features_std[j] = (self.features_std[j] / x.len() as f64 - self.features_mean[j] * self.features_mean[j]).sqrt();
+        }
+
+        let mut standardized = vec![vec![0.0; x[0].len()]; x.len()];
+        for (i, sample) in x.iter().enumerate() {
+            for (j, &value) in sample.iter().enumerate() {
+                standardized[i][j] = (value - self.features_mean[j]) / self.features_std[j];
+            }
+        }
+        standardized
     }
 
     /// Returns the weights of the model.
@@ -185,6 +235,51 @@ impl LinearRegressor {
 pub mod tests {
     use super::*;
 
+
+    #[test]
+    fn test_high_variance_multi_params_func() {
+        // linear function: y = 3x1 - 1x2 -6x3 + 2
+        let mut model: LinearRegressor = 
+            LinearRegressor::new()
+                .set_loss_threshold(1e-5)
+                .set_gradient_threshold(1e-5)
+                .set_max_epochs(1_000_000)
+                .set_learning_rate(0.01);
+        let x = vec![
+            vec![100.0, -3.2, -2.0], 
+            vec![-300.0, 2000.5, 13.0], 
+            vec![5647.0, 1.0, -30.0], 
+            vec![-20072.0, 523.0, 142.5], 
+            vec![13.0, -123.0, 1321.0]
+        ];
+        let y: Vec<f64> = x.iter()
+            .map(|params| 3. * params[0] - 1. * params[1] - 6. * params[2] + 2.)
+            .collect();
+        let loss = model.fit(&x, &y).unwrap();
+        let weights = model.get_weights().unwrap();
+        let bias = model.get_bias().unwrap();
+        println!("test params: [3, 1, -6, 2]");
+        println!("weights: {:?}", weights);
+        println!("bias: {}", bias);
+        println!("loss: {}", loss);
+        assert!(loss < 1e-4);
+        
+        // Test prediction
+        let predictions = model.predict(&vec![vec![7.0, -3.1, 2.3], vec![-4.0, 1.3, -2.1]]).unwrap();
+        let expected_predictions = vec![
+            3. * 7.0 + -1. * -3.1 -6. * 2.3 + 2., 
+            3. * -4.0 + -1. * 1.3 -6. * -2.1 + 2.
+        ];
+        let prediction_loss: f64 = predictions.iter()
+            .zip(expected_predictions.iter())
+            .map(|(pred, expected)| (pred - expected) * (pred - expected))
+            .sum();
+        println!("predictions: {:?}", predictions);
+        println!("expected predictions: {:?}", expected_predictions);
+        println!("prediction loss: {}", prediction_loss);
+        assert!(prediction_loss < 1e-3);
+
+    }
 
     #[test]
     fn test_linear_func() {
@@ -262,5 +357,39 @@ pub mod tests {
 
     }
     
-    
+    #[test]
+    fn test_invalid_input() {
+        let mut model: LinearRegressor = LinearRegressor::new();
+        let x = vec![vec![1.0, 2.0], vec![3.0, 4.0]];
+        let y = vec![1.0, 2.0, 3.0];
+        assert_eq!(model.fit(&x, &y), Err(Error::InvalidInput));
+    }
+
+    #[test]
+    fn test_already_fitted() {
+        let mut model: LinearRegressor = LinearRegressor::new();
+        let x = vec![vec![1.0, 2.0], vec![3.0, 4.0]];
+        let y = vec![1.0, 2.0];
+        model.fit(&x, &y).unwrap();
+        assert_eq!(model.fit(&x, &y), Err(Error::AlreadyFitted));
+    }
+
+    #[test]
+    fn test_not_fitted() {
+        let model: LinearRegressor = LinearRegressor::new();
+        let x = vec![vec![1.0, 2.0], vec![3.0, 4.0]];
+        assert_eq!(model.predict(&x), Err(Error::NotFitted));
+    }
+
+    #[test]
+    fn test_get_weights_not_fitted() {
+        let model: LinearRegressor = LinearRegressor::new();
+        assert_eq!(model.get_weights(), Err(Error::NotFitted));
+    }
+
+    #[test]
+    fn test_get_bias_not_fitted() {
+        let model: LinearRegressor = LinearRegressor::new();
+        assert_eq!(model.get_bias(), Err(Error::NotFitted));
+    }
 }
